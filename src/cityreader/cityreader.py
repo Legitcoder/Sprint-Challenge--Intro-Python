4cec472073424fcac5d3dac8aef033a3ae63bea3
Moin Uddin <U_Moin75@yahoo.com>
2019-02-15 13:42:17 -0500

    Attempt Stretch

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -39,8 +39,8 @@ def cityreader(cities=[]):
 cityreader(cities)
 
 # Print the list of cities (name, lat, lon), 1 record per line.
-for c in cities:
-    print(f'({c.name}, {c.lat}, {c.lon})')
+#for c in cities:
+    #print(f'({c.name}, {c.lat}, {c.lon})')
 
 # STRETCH GOAL!
 #
@@ -72,13 +72,24 @@ for c in cities:
 # Salt Lake City: (40.7774,-111.9301)
 
 # TODO Get latitude and longitude values from the user
+latOne,longOne = map(float, input("Enter lat1,lon1: ").split(','))
+latTwo,longTwo = map(float, input("Enter lat2,lon2: ").split(','))
+
+if latOne > latTwo:
+  latOne, latTwo = latTwo, latOne
+
+if longOne > longTwo:
+  longOne, longTwo = longTwo, longOne
 
 def cityreader_stretch(lat1, lon1, lat2, lon2, cities=[]):
   # within will hold the cities that fall within the specified region
-  within = []
-
+  within = [city for city in cities if city.lat > lat1 and city.lat < lat2 and city.lon > lon1 and city.lon < lon2]
+  for c in within:
+    print(f'{c.name}: {c.lat}, {c.lon}')
   # TODO Ensure that the lat and lon valuse are all floats
   # Go through each city and check to see if it falls within 
   # the specified coordinates.
 
   return within
+
+cityreader_stretch(latOne, longOne, latTwo, longTwo, cities)
